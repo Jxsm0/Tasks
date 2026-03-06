99727a479c79e4b826ff52b4062ee7cbd60eb639
Jxsm0 <juliusschaper20@gmail.com>
2026-03-06 11:39:50 +0100

    Add user input for name and age in hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+"""
+request users name, then print it
+request users age, then print it
+print hello world
+"""
+user_name = input("Please enter your name: ")
+print(user_name)
+user_age = input("Please enter your age : ")
+print(user_age)
+print("Hello, World")
